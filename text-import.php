c8bb44c0c9e5fb6b4f6dda4078025844376b0564
onion <a0966550619@gmail.com>
2025-02-21 10:31:50 +0800

    匯入CSV文字檔,並以html 表格方式以頁面呈現內容

--- text-import.php
+++ text-import.php
@@ -1,3 +1,19 @@
+<style>
+    table{
+        border-collapse:collapse;
+        border-spacing:0;
+        border:1px solid #ccc;
+        margin:10px 0;
+    }
+    table th,table td{
+        border:1px solid #ccc;
+        padding:5px;
+    }
+    .header{
+        text-align:center;
+    }
+</style>
+
 <?php
 /****
  * 1.建立資料庫及資料表
@@ -8,6 +24,36 @@
  * 6.寫入資料庫
  * 7.結束檔案
  */
+
+if(!empty($_FILES['file'])){
+    move_uploaded_file($_FILES['file']['tmp_name'], "./files/{$_FILES['file']['name']}");
+    echo $_FILES['file']['name']."上傳成功";
+    getfile("./files/{$_FILES['file']['name']}");
+}
+
+
+function getfile($path){
+    $file=fopen($path,'r');
+    $line=fgets($file);
+    $cols=explode(",",trim($line));
+    echo "<table>";
+    echo "<tr class='header'>";
+    foreach($cols as $col){
+        echo "<th>{$col}</th>";
+    }
+    echo "</tr>";
+
+    while($line=fgets($file)){
+        $cols=explode(",",trim($line));
+        echo "<tr>";
+        foreach($cols as $col){
+            echo "<td>{$col}</td>";
+        }
+        echo "</tr>";
+    }
+
+    echo "</table>";
+}
 
 ?>
 <!DOCTYPE html>
@@ -22,11 +68,17 @@
 <body>
 <h1 class="header">文字檔案匯入練習</h1>
 <!---建立檔案上傳機制--->
-
+<form action="?" method="post" enctype="multipart/form-data">
+    <label for="file">文字檔:</label><input type="file" name="file" id="file">
+    <input type="submit" value="上傳">
+</form>
 
 
 <!----讀出匯入完成的資料----->
+<?php 
 
+
+?>
 
 
 </body>
